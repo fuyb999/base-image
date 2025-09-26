c64a9cbb7fa56bcf1f5307ce981d0e7739e04b02
fuyb <656452024@qq.com>
2025-09-26 20:51:50 +0800

    run script 1000

--- build/COPY_ROOT_0/etc/cont-init.d/05-init-source-preflight-scripts.sh
+++ build/COPY_ROOT_0/etc/cont-init.d/05-init-source-preflight-scripts.sh
@@ -7,10 +7,13 @@
 set -e # Exit immediately if a command exits with a non-zero status.
 set -u # Treat unset variables as an error.
 
+sudo chown -R ${USER_ID}:${GROUP_ID} /opt/ai-dock/etc/
+
 preflight_dir="/opt/ai-dock/bin/preflight.d"
 printf "Looking for scripts in %s...\n" "$preflight_dir"
 for script in /opt/ai-dock/bin/preflight.d/*.sh; do
   source "$script";
 done
+
 
 # vim:ft=sh:ts=4:sw=4:et:sts=4
--- build/COPY_ROOT_0/opt/ai-dock/etc/environment.sh
+++ build/COPY_ROOT_0/opt/ai-dock/etc/environment.sh
@@ -1,6 +1,6 @@
 # BUILDTIME INSTRUCTIONS
 
-if [[ "$(id -u)" == "${USER_ID}" ]] && [[ ! -f ${HOME}/.gitconfig ]]; then
+if [[ "$(id -u)" == "${USER_ID}" && ! -f ${HOME}/.gitconfig ]]; then
     git config --global --add safe.directory "*"
 fi
 
